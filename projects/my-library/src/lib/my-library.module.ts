490537621a727272decf0640e03102545a8b75fc
Krmannu23 <117580538+Krmannu23@users.noreply.github.com>
2024-02-11 11:07:07 +0530

    Module addition

--- projects/my-library/src/lib/my-library.module.ts
+++ projects/my-library/src/lib/my-library.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { MyLibraryComponent } from './my-library.component';
+import { UserAuthenticationModule } from './user-authentication/user-authentication.module';
+import { UserAcademicModule } from './user-academic/user-academic.module';
+import { UserDetailsModule } from './user-details/user-details.module';
+import { UserPerformanceModule } from './user-performance/user-performance.module';
 
 
 
@@ -8,6 +12,10 @@ import { MyLibraryComponent } from './my-library.component';
     MyLibraryComponent
   ],
   imports: [
+    UserAuthenticationModule,
+    UserAcademicModule,
+    UserDetailsModule,
+    UserPerformanceModule
   ],
   exports: [
     MyLibraryComponent
