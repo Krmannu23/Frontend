87b4e70b601c93ea3882eda35581ff87a9dfbcb6
Krmannu23 <117580538+Krmannu23@users.noreply.github.com>
2024-02-11 11:24:11 +0530

    second phase of component creation

--- projects/my-library/src/lib/user-authentication/user-authentication.module.ts
+++ projects/my-library/src/lib/user-authentication/user-authentication.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { UserregisterComponent } from './userregister/userregister.component';
+import { UserlogoutComponent } from './userlogout/userlogout.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserloginComponent,
+    UserregisterComponent,
+    UserlogoutComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ projects/my-library/src/lib/user-authentication/userlogout/userlogout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-userlogout',
+  templateUrl: './userlogout.component.html',
+  styleUrls: ['./userlogout.component.css']
+})
+export class UserlogoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
