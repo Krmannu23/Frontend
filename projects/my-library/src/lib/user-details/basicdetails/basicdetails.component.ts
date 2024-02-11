72b2d27f1c2189d4d78774661a0b822f0ce5dd36
Krmannu23 <117580538+Krmannu23@users.noreply.github.com>
2024-02-11 11:20:27 +0530

    first phase of component creation

---
+++ projects/my-library/src/lib/user-details/basicdetails/basicdetails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-basicdetails',
+  templateUrl: './basicdetails.component.html',
+  styleUrls: ['./basicdetails.component.css']
+})
+export class BasicdetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
